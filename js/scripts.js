057f69c23623846046e5dec7fb999901ab245a6d
David Boedigheimer <stealth259@gmail.com>
2019-08-22 12:43:17 -0700

    Add 3 varables and 3 new functions to scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 var pound = prompt("How many Pounds?")
+var kilogram = (2.205);
+var stone = (14);
+var ounce = (16);
+
+var kilogramEquation = function(number1, number2) {
+  return number1 / number2;
+};
+
+var stoneEquation = function(number1, number2) {
+  return number1 / number2;
+};
+
+var ounceEquation = function(number1, number2) {
+  return number1 * number2;
+};
+
+var result = kilogramEquation(pound, kilogram);
+alert(result);
