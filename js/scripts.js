0a6605900f15f51d8b893ba23323763595712e05
David Boedigheimer <stealth259@gmail.com>
2019-09-18 13:26:00 -0700

    Add submit functions.

--- js/scripts.js
+++ js/scripts.js
@@ -12,5 +12,24 @@ var ounceEquation = function(num) {
 };
 
 $(document).ready(function() {
+  $("form#kilos").submit(function(event) {
+    event.preventDefault();
+    var num = parseInt($("#kilo").val());
+    var result = kilogramEquation(num);
+    $("#ansKilo").text(result);
+  });
 
+  $("form#stones").submit(function(event) {
+    event.preventDefault();
+    var num = parseInt($("#stone").val());
+    var result = stoneEquation(num);
+    $("#ansStone").text(result);
+  });
+
+  $("form#ounces").submit(function(event) {
+    event.preventDefault();
+    var num = parseInt($("#ounce").val());
+    var result = ounceEquation(num);
+    $("#ansOunce").text(result);
+  });
 });
