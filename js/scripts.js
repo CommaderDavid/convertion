b194c0f817a9d2cbae004dfe7fa7470b4f957f6f
David Boedigheimer <stealth259@gmail.com>
2019-09-18 11:45:53 -0700

    Add 3 different convertions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
-var kilogram = (2.205);
-var stone = (14);
-var ounce = (16);
 
-var kilogramEquation = function(number1, number2) {
-  return number1 / number2;
+var kilogramEquation = function(num) {
+  return num / 2.205;
 };
 
-var stoneEquation = function(number1, number2) {
-  return number1 / number2;
+var stoneEquation = function(num) {
+  return num / 14;
 };
 
-var ounceEquation = function(number1, number2) {
-  return number1 * number2;
+var ounceEquation = function(num) {
+  return num * 16;
 };
+
+$(document).ready(function() {
+
+});
